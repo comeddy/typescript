973b3714a278e49d09a8ae7a551b6153cf0e40eb
oscar <comeddy@gmail.com>
2019-12-31 09:58:31 +0900

    #3 tsc-watch Setting

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
-};
-console.log(sayHi("Youngjin Kim", 47, "male"));
-//# sourceMappingURL=index.js.map
