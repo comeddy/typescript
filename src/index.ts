883a2aed1342d5809eaed356c253e1b0efb28dc2
oscar <comeddy@gmail.com>
2019-12-31 10:08:30 +0900

    #4 using Object/inferface Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "Youngjin Kim",
+    age: 47,
+    gender: "male"
 };
 
-console.log(sayHi("Youngjin Kim", 47, "male"));
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(person));
 
 export {};
